62bbd381d01ca4660e33ae4fdf7c96bf04108fe4
sota2000 <139357317+sota2000@users.noreply.github.com>
2023-07-13 23:05:38 +0900

    Update lab03.R

--- lab03.R
+++ lab03.R
@@ -1,3 +1,16 @@
-Check something is on your laptop:
+print("I have created my first repository!")
+print("My name on GitHub is set to:")
+system("git config sota2000")
+print("My e-mail on GitHub is set to:")
+system("git config user.email")
+
+
+
+
+
+
+
+
+
 
 Git version
